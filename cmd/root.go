9be663097f4c8ac348a60899989aef2d16c1833a
Matt Krueger <mkrueger@rstms.net>
2025-08-30 03:01:28 -0600

    Bump version: 0.0.2 → 0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -39,7 +39,7 @@ import (
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-	Version: "0.0.2",
+	Version: "0.0.3",
 	Use:     "boxen-template",
 	Short:   "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
